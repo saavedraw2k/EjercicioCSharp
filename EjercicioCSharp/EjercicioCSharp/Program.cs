6dcdff361622252110d2d42111cc2cccbc0125ed
GABRIEL ANTONIO SAAVEDRA MARTINEZ <GabSaavStud@nube.unadmexico.mx>
2022-02-28 01:27:32 -0600

    Finish date class

---
+++ EjercicioCSharp/EjercicioCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using EjercicioCSharp;
+
+Console.WriteLine("Conceptos de POO");
+Console.WriteLine("=================");
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1985, 11, 31));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
